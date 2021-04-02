ee4eb91f4222831e016d831a3ddc67147449b77b
bewakes <bewakepandey@gmail.com>
2021-04-02 12:33:45 +0545

    Add as-is output and change state object

---
+++ unicode_converter/convert.py
@@ -0,0 +1,84 @@
+from dataclasses import dataclass
+from typing import List, TypeVar, Tuple
+
+from unicode_converter.mappings import (
+    get_mappings,
+    consonant_kaars,
+    get_word_maps,
+    Mappings,
+    amkaar,
+    aNNkaar,
+    Ri,
+)
+
+
+@dataclass
+class State:
+    remaining: str = ''
+    consumed: str = ''
+    processed: str = ''
+    as_is: bool = False
+
+    def copy(self, **kwargs):
+        return State(self.remaining, self.consumed, self.processed, **kwargs)
+
+
+class Converter:
+    def __init__(self):
+        self.mappings = get_mappings()
+        self.word_maps = get_word_maps()
+
+    def consume(self, state: State) -> State:
+        current = state.remaining
+        consumed = state.consumed
+        processed = state.processed
+
+        if state.as_is and current[0] == '}':
+            return State(current[1:], consumed + current[:1], processed, False)
+        if state.as_is and current[0] != '}':
+            return State(current[1:], consumed + current[0], processed + current[0], True)
+
+        # check for escape sequences
+        if current.startswith('{{'):
+            return State(current[2:], consumed + current[:2], processed+'{')
+        if current.startswith('{') and not state.as_is:
+            return State(
+                current[1:],
+                consumed + current[:1],
+                processed,
+                True
+            )
+        # add amkar and aaNkar
+        if current.startswith('M'):
+            return State(current[1:], consumed + current[0], processed + amkaar)
+        if current.startswith('NN'):
+            return State(current[2:], consumed + current[:2], processed + aNNkaar)
+
+        # Special case for Ri and Ree
+        if current.startswith('RI') and consumed[-1] != 'a':
+            # remove halanta and add Ri
+            return State(current[2:], consumed + current[:2], processed[:-1] + Ri)
+        if current.startswith('RI') and consumed[-1] == 'a':
+            # normal proceeding
+            return State(current[2:], consumed + current[:2], processed + Ri)
+
+        for k, v in self.mappings.items():
+            if current.startswith(k):
+                return State(current[len(k):], consumed + current[:len(k)], processed+v)
+        else:
+            # TODO: what if upper case and does not match any pattern?
+            # In that case try lower casing char by char
+            return State(current[1:], consumed + current[0], processed+current[0])
+
+    def convert(self, text: str) -> str:
+        if not text:
+            return text
+        # Check if word is in direct word mappings
+        for k, v in self.word_maps.items():
+            if text.startswith(k):
+                return self.word_maps[k] + self.convert(text[len(k):])
+
+        state = State(text)
+        while state.remaining:
+            state = self.consume(state)
+        return state.processed
---
+++ unicode_converter/tests/test_conversion.py
@@ -0,0 +1,32 @@
+from unicode_converter.convert import Converter
+
+
+test_cases = [
+    ('gaahro', 'गाह्रो'),
+    ('aaNNkhaa', 'आँखा'),
+    ('samRIddha', 'समृद्ध'),
+    ('samaRIddha', 'समृद्ध'),
+    ('garyo', 'गर्यो'),
+    ('hudai', 'हुँदै'),
+    ('instagram', 'इन्स्टाग्राम'),
+    ('aum nama: shiwaaya', 'ॐ नम: शिवाय'),
+    ('kaam chhaina timro, haNN?', 'काम् छैन तिम्रो, हँ?'),
+    ('messengermaa message aayo', 'मेसेन्जरमा मेस्सगे आयो'),
+    ('aNDaa', 'अण्डा'),
+    ('kaMsha', 'कंश'),
+    ('aNNgaara', 'अँगार'),
+    ('yuNNs', 'युँस्'),
+    ('agyaanee', 'अज्ञानी'),
+    ('gYaarej', 'ग्यारेज्'),
+    ('ma RiShi ho.', 'म ॠ षि हो।'),
+    ('ma {in} gYaarej', 'म in ग्यारेज्'),
+    ('ma {{in} gYaarej', 'म {इन्} ग्यारेज्'),
+    ('ma {{{in}} gYaarej', 'म {in} ग्यारेज्'),
+    # TODO: figure out what to do with the next case
+    # ('ma {inside {of} the } gYaarej', 'म inside {of}  ग्यारेज्'),
+]
+
+def test_conversion():
+    converter = Converter()
+    for roman, expected in test_cases:
+        assert converter.convert(roman) == expected
